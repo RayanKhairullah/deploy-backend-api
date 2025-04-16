f250432562a9e8efcd5a8630d305be396461d4e6
ryn <raynrayan1341@gmail.com>
2025-04-16 07:38:15 +0700

    selese

--- api/index.js
+++ api/index.js
@@ -5,8 +5,6 @@ const routes = require('../routes/expenseRoutes');
 const authRoutes = require('../routes/authRoutes');
 const prisma = require('../utils/prismaClient');
 const errorHandler = require('../middlewares/errorHandler');
-const Inert = require('@hapi/inert');
-const path = require('path');
 
 let server;
 
@@ -23,16 +21,6 @@ async function initServer() {
           exposedHeaders: ['WWW-Authenticate', 'Server-Authorization'],
           maxAge: 86400
         }
-      }
-    });
-
-    await server.register(Inert);
-
-    server.route({
-      method: 'GET',
-      path: '/',
-      handler: {
-        file: path.join(__dirname, '../docs/docs.html')
       }
     });
 
@@ -72,15 +60,14 @@ module.exports = async (req, res) => {
       payload: req.body,
     });
 
-    res.setHeader('Content-Type', response.headers['content-type']);
-    Object.entries(response.headers).forEach(([key, value]) => {
-      if (key.toLowerCase() !== 'content-type') { // Avoid duplicate
-        res.setHeader(key, value);
-      }
-    });
+    Object.entries(response.headers)
+    .filter(([key]) => key.toLowerCase() !== 'content-encoding')
+    .forEach(([key, value]) => {
+      res.setHeader(key, value);
+    });  
 
-    // Send the correct status and payload
-    res.status(response.statusCode).send(response.result);
+    // Send status and result
+    res.status(response.statusCode).json(response.result);
   } catch (error) {
     console.error('Error processing request:', error);
     res.status(500).json({ 
